4c2a50ae96acac64ae6f18bd07edb5f0e88205ba
andrearicciotti <ricciotti.andrea1993@gmail.com>
2024-01-15 14:40:22 +0100

    add: query for get all movies in HomeController file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::all();
+        dd($movies);
+
+        return view('home', compact('movies'));
+    }
 }
