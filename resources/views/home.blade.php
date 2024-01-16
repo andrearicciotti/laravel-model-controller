6d75b330a899da5307987a6cfd25baac0e9511da
andrearicciotti <ricciotti.andrea1993@gmail.com>
2024-01-16 02:33:16 +0100

    add bootstrap and scss style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,15 @@
 @section('movies')
 
 <div class="container text-center">
-    <h1>Movies</h1>
+    <h1 class="my-3">Movies</h1>
 
-    <div class="row row-cols-4">
+    <div class="row g-2">
         @foreach ($movies as $movie)
-            <div class="col">
+            <div class="col-3">
                 <div class="card" style="width: 18rem;">
                     <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }} image">
                     <div class="card-body">
-                        <h3 class="card-title">Title: {{ $movie->title }}</h3>
+                        <h4 class="card-title">Title: {{ $movie->title }}</h4>
                         <h5>Nationality: {{ $movie->nationality }}</h5>
                         <p><strong>Release date:</strong> {{ $movie->date }}</h5>
                         <p><strong>Vote average:</strong> {{ $movie->vote }}</p>
